41bd3ac01b887e2dc336157fc597e2ca5dd3c58d
Hoàng Lê <Hong L>
2023-10-25 23:39:52 +0700

    edit code

--- FrontEnd/client/src/components/AuthModal/ForgetPassword/ForgetPassword.js
+++ FrontEnd/client/src/components/AuthModal/ForgetPassword/ForgetPassword.js
@@ -1,54 +1,53 @@
-import React, { Component } from 'react';
-import {Row, Col, Form, Input, Button, Spin} from 'antd';
+import React from 'react';
+import { Row, Col, Form, Input, Button, Spin } from 'antd';
 import FormBuilder from 'antd-form-builder';
-import { UserOutlined} from '@ant-design/icons';
+import { UserOutlined } from '@ant-design/icons';
 
-export default class ForgetPassword extends Component {
-   
-    formForget = FormBuilder.createForm(this);
-    onFinish(values){
-        this.props.onForgot(values.email);
-        this.formForget.resetFields();
-    }
-    
-    render() {
-        return (
-            <div>
-                
-                <Form
-                form={this.formForget}
-                className="login-form"
-                    initialValues={{
-                        email: '',
-                    }}
-                    onFinish={this.onFinish.bind(this)}
-                >
-                    <Form.Item
-                    name="email"
-                    label="Email"
-                    rules={[
-                        {
-                          type: 'email',
-                          message: 'Nhập email không hợp lệ!',
-                        },
-                        {
-                          required: true,
-                          message: 'Xin vui lòng nhập email',
-                        },
-                        ]}
-                        hasFeedback
-                        
-                   >
-                       <Input placeholder="Nhập Email"/>
-                   </Form.Item>
-                   <Row>
-                        <Col lg={{span: 4, offset: 10}}>
-                            <Button htmlType="submit" type="primary">Xác nhận</Button>
-                        </Col>
-                   </Row>
-                </Form>
-                
-            </div>
-        )
-    }
-}
+const ForgetPassword = ({ onForgot }) => {
+  const formForget = FormBuilder.createForm();
+
+  const onFinish = (values) => {
+    onForgot(values.email);
+    formForget.resetFields();
+  };
+
+  return (
+    <div>
+      <Form
+        form={formForget}
+        className="login-form"
+        initialValues={{
+          email: '',
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="email"
+          label="Email"
+          rules={[
+            {
+              type: 'email',
+              message: 'Nhập email không hợp lệ!',
+            },
+            {
+              required: true,
+              message: 'Xin vui lòng nhập email',
+            },
+          ]}
+          hasFeedback
+        >
+          <Input placeholder="Nhập Email" />
+        </Form.Item>
+        <Row>
+          <Col lg={{ span: 4, offset: 10 }}>
+            <Button htmlType="submit" type="primary">
+              Xác nhận
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </div>
+  );
+};
+
+export default ForgetPassword;
--- FrontEnd/client/src/components/AuthModal/ResetPassword/ResetPassword.js
+++ FrontEnd/client/src/components/AuthModal/ResetPassword/ResetPassword.js
@@ -1,160 +1,147 @@
-import React, { Component } from 'react';
-import {Row, Col, Form, Input, Button, message} from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Row, Col, Form, Input, Button, message } from 'antd';
 import queryString from 'query-string';
-import {Redirect} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import axiosInstance from '../../../utils/axiosInstance';
 
-
 const formItemLayout = {
-    labelCol: {
-        xs: {
-            span: 24,
-        },
-        sm: {
-            span: 8,
-        }
+  labelCol: {
+    xs: {
+      span: 24,
     },
-    wrapperCol: {
-        xs: {
-            span: 24,
-        },
-        sm: {
-            span: 16,
-        },
+    sm: {
+      span: 8,
     },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
 };
-const containerStyle = {
-    border: '1px solid #f0f0f0',
-    padding: 20,
-    borderRadius: 5,
-    background: '#f0f0f0'
-}
-const titleStyle = {
-    textAlign: 'center',
-    padding: 15,
-}
 
-export default class ResetPassword extends Component {
-    constructor(props){
-        super();
-        this.state = {
-            token: null,
-            email: null,
-            isSuccess: false,
+const containerStyle = {
+  border: '1px solid #f0f0f0',
+  padding: 20,
+  borderRadius: 5,
+  background: '#f0f0f0',
+};
+
+const titleStyle = {
+  textAlign: 'center',
+  padding: 15,
+};
+
+const ResetPassword = (props) => {
+  const [token, setToken] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  useEffect(() => {
+    const query = queryString.parse(props.location.search);
+    setToken(query.token);
+    setEmail(query.email);
+    setIsSuccess(false);
+  }, [props.location.search]);
+
+  const handleSubmit = async (values) => {
+    if (!!token && !!email) {
+      const body = {
+        token: token.split(' ').join('+'),
+        email: email,
+        newPassword: values.password,
+      };
+
+      try {
+        const res = await axiosInstance('User/ResetPassword', 'POST', body);
+
+        if (res.data) {
+          message.success('Reset Password thành công!', 3);
+          setIsSuccess(true);
+        } else {
+          message.warning('Reset Password thất bại!', 3);
+          setIsSuccess(true);
         }
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      setIsSuccess(true);
     }
-    componentDidMount(){
-        var query = queryString.parse(this.props.location.search);
-        this.setState({
-            token: query.token,
-            email: query.email,
-            isSuccess: false,
-        })
-    }
-    async handleSubmit(values) {
-        const {token, email} = this.state;
-        if(!!token && !!email){
-            const body = {
-                token: token.split(' ').join('+'),
-                email: email,
-                newPassword: values.password,
-            }
-            console.log(body);
-            await axiosInstance('User/ResetPassword', 'POST', body)
-            .then(res => {
-                if(res.data){
-                    message.success('Reset Password thành công!', 3);
-                    return <Redirect to="/"></Redirect>
-                }
-                else{
-                    message.warning('Reset Password thất bại!', 3);
-                    return <Redirect to="/"></Redirect>
-                }
-                
-            })
-            
-        }
-        else{
-            return <Redirect to="/"></Redirect>
-        }
-        
-    }
-    render() {
-        if(this.state.isSuccess === false){
-        return (
-            
-            <Row>
-                <Col lg={{span: 8, offset: 8}} style={{padding: 10}}>
-                    <div style={containerStyle}>
-                        <h3 style={titleStyle}>QUÊN MẬT KHẨU</h3>
-                        <hr/>
-                        <br></br>
-                        <br></br>
-                        <Form
-                        onFinish={this.handleSubmit.bind(this)}
-                        {...formItemLayout}
-                        >
-                            
-                            <Form.Item
-                            
-                            name="password"
-                            label="Mật khẩu mới"
-                            rules={[
-                            {
-                            required: true,
-                            message: 'Xin vui lòng nhập mật khẩu!',
-                            },
-                            {
-                                min: 10,
-                                message: 'Mật khẩu phải có tối thiểu 10 ký tự!'
-                            }
-                            ]}
-                            hasFeedback
-                            >
-                                <Input.Password/>
-                            </Form.Item>
-                            <Form.Item
-                            name="confirm"
-                            label="Nhập lại mật khẩu"
-                            dependencies={['password']}
-                            rules={[
-                            {
-                            required: true,
-                            message: 'Xin vui lòng nhập lại mật khẩu!',
-                            },
-                            {
-                                min: 8,
-                                message: 'Mật khẩu phải có tối thiểu 8 ký tự!'
-                            },
-                            ({ getFieldValue }) => ({
-                            validator(rule, value) {
-                              if (!value || getFieldValue('password') === value) {
-                                return Promise.resolve();
-                              }
-                
-                              return Promise.reject('Nhập lại mật khẩu chưa khớp với mật khẩu ở trên!');
-                            },
-                            })
-                                ]}
-                                hasFeedback
-                            >
-                                <Input.Password/>
-                            </Form.Item>
-                            <br></br>
-                            <div style={{textAlign: 'center'}}>
-                                <Button htmlType="submit" type="primary">Xác nhận</Button>
-                            </div>
-                            <br></br>
-                        </Form>
-                    </div>
-                </Col>
-                    
-            </Row>
-            
-        )
-        }
-        else{
-            return <Redirect to="/"></Redirect>
-        }
-    }
-}
+  };
+
+  if (!isSuccess) {
+    return (
+      <Row>
+        <Col lg={{ span: 8, offset: 8 }} style={{ padding: 10 }}>
+          <div style={containerStyle}>
+            <h3 style={titleStyle}>QUÊN MẬT KHẨU</h3>
+            <hr />
+            <br></br>
+            <br></br>
+            <Form onFinish={handleSubmit} {...formItemLayout}>
+              <Form.Item
+                name="password"
+                label="Mật khẩu mới"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Xin vui lòng nhập mật khẩu!',
+                  },
+                  {
+                    min: 10,
+                    message: 'Mật khẩu phải có tối thiểu 10 ký tự!',
+                  },
+                ]}
+                hasFeedback
+              >
+                <Input.Password />
+              </Form.Item>
+              <Form.Item
+                name="confirm"
+                label="Nhập lại mật khẩu"
+                dependencies={['password']}
+                rules={[
+                  {
+                    required: true,
+                    message: 'Xin vui lòng nhập lại mật khẩu!',
+                  },
+                  {
+                    min: 8,
+                    message: 'Mật khẩu phải có tối thiểu 8 ký tự!',
+                  },
+                  ({ getFieldValue }) => ({
+                    validator(rule, value) {
+                      if (!value || getFieldValue('password') === value) {
+                        return Promise.resolve();
+                      }
+
+                      return Promise.reject('Nhập lại mật khẩu chưa khớp với mật khẩu ở trên!');
+                    },
+                  }),
+                ]}
+                hasFeedback
+              >
+                <Input.Password />
+              </Form.Item>
+              <br></br>
+              <div style={{ textAlign: 'center' }}>
+                <Button htmlType="submit" type="primary">
+                  Xác nhận
+                </Button>
+              </div>
+              <br></br>
+            </Form>
+          </div>
+        </Col>
+      </Row>
+    );
+  } else {
+    return <Redirect to="/" />;
+  }
+};
+
+export default ResetPassword;
